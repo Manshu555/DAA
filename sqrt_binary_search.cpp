88be2bdb349d612f29651bc442f4c145cc08fdcd
Manshu Jaiswal <145484692+Manshu555@users.noreply.github.com>
2024-09-18 11:11:18 +0530

    Update sqrt_binary_search.cpp

--- sqrt_binary_search.cpp
+++ sqrt_binary_search.cpp
@@ -1,18 +1,44 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// long long sqrt_num(long long l, long long h, long long n) {
+//     long long mid = (l + h) / 2;
+//     if (mid * mid == n) return mid;
+//     if (h - l == 1) return l;
+//     if (mid * mid > n)
+//         return sqrt_num(l, mid, n);
+//     else
+//         return sqrt_num(mid, h, n);
+// }
+
+// int main() {
+//     long long n;
+//     cin >> n;
+//     cout << sqrt_num(0, n, n);
+//     return 0;
+// }
+
+
 #include <bits/stdc++.h>
 using namespace std;
-long long sqrt_num(long long l, long long h, long long n) {
-    long long mid = (l + h) / 2;
-    if (mid * mid == n) return mid;
-    if (h - l == 1) return l;
-    if (mid * mid > n)
-        return sqrt_num(l, mid, n);
-    else
-        return sqrt_num(mid, h, n);
+
+double bs(double n){
+    double l = 0;
+    double h = n;
+    while((h - l) > 1e-6){
+        double mid = (l + h) / 2.0; 
+        if (mid * mid > n) h = mid;
+        else l = mid;
+    }
+    return (l + h) / 2.0; 
 }
 
-int main() {
-    long long n;
+int main(){
+    double n;
     cin >> n;
-    cout << sqrt_num(0, n, n);
+    cout <<setprecision(6) << bs(n);
     return 0;
 }
+
+
+
+
