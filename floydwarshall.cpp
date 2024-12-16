6cdfbbd5c3174601e3276c9a454f9137a0e76ada
Manshu Jaiswal <145484692+Manshu555@users.noreply.github.com>
2024-12-16 11:05:51 +0530

    Update floydwarshall.cpp

--- floydwarshall.cpp
+++ floydwarshall.cpp
@@ -1,52 +1,41 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-#define V 4
-
-#define INF 99999
-
-void printSolution(int dist[][V])
-{
-    cout << "The following matrix shows the shortest "
-            "distances"
-            " between every pair of vertices \n";
-    for (int i = 0; i < V; i++) {
-        for (int j = 0; j < V; j++) {
-            if (dist[i][j] == INF)
-                cout << "INF"
-                     << " ";
-            else
-                cout << dist[i][j] << "   ";
+class Graph {
+public:
+    vector <vector <int>> adj;
+    int N;
+    Graph(int n, vector<vector<int>>& edges) {
+        N = n;
+        adj.resize(n, vector<int>(n, 1e9));
+        for(auto &edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            int c = edge[2];
+            adj[u][v] = c;
         }
-        cout << endl;
-    }
-}
 
-void floydWarshall(int dist[][V])
-{
+        for(int i=0;i<n;i++) adj[i][i]= 0;
 
-    int i, j, k;
-    for (k = 0; k < V; k++) {
-        for (i = 0; i < V; i++) {
-            for (j = 0; j < V; j++) {
-                if (dist[i][j] > (dist[i][k] + dist[k][j])
-                    && (dist[k][j] != INF
-                        && dist[i][k] != INF))
-                    dist[i][j] = dist[i][k] + dist[k][j];
+        //floydwarshall
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    adj[i][j] = min(adj[i][j] , adj[i][k] + adj[k][j]);
+                }
             }
         }
     }
-    printSolution(dist);
-}
-
-int main()
-{   
-    int graph[V][V] = { {0,3,INF,5},
-                        {2,0,INF,4},
-                        {INF,1,0,INF},
-                        { INF, INF,2, 0 } };
-
-
-    floydWarshall(graph);
-    return 0;
-}
+    
+    void addEdge(vector<int> edge) {
+        int u = edge[0];
+        int v = edge[1];
+        int cost = edge[2];
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                adj[i][j] = min(adj[i][j] , adj[i][u] + cost + adj[v][j]);
+            }
+        }
+    }
+    int shortestPath(int node1, int node2) {
+        int x = adj[node1][node2];
+        return (x == 1e9) ?  -1 : x; 
+    }
+};
